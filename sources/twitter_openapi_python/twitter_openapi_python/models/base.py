ec3c2dfb99382e61722a3e2d512d58d6d38eaeac
wuaikaiyuan <lsc_000@126.com>
2025-01-08 15:15:32 +0800

    1、删除无用文件；2、添加Twitter api docs；

--- sources/twitter_openapi_python/twitter_openapi_python/models/base.py
+++
@@ -1,9 +0,0 @@
-from typing import TypeVar
-
-from pydantic import BaseModel as PydanticBaseModel
-
-T = TypeVar("T")
-
-
-class BaseModel(PydanticBaseModel):
-    model_config = {"arbitrary_types_allowed": True}
--- sources/twitter_openapi_python_generated/tools/win/publish.ps1
+++
@@ -1,7 +0,0 @@
-pip install wheel twine setuptools
-Remove-Item twitter_openapi_python_generated.egg-info/*
-Remove-Item dist/*
-python setup.py sdist
-python setup.py bdist_wheel
-Read-Host 'Press Enter to continue'
-twine upload dist/* --verbose
